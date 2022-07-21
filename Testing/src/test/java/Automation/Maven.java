01c446f26a3972b5e109eec28fd9ee571e1e7b2c
HP <HP@LAPTOP-DR1P4N5S>
2022-07-21 14:56:22 +0530

    first

---
+++ Testing/src/test/java/Automation/Maven.java
@@ -0,0 +1,11 @@
+package Automation;
+
+import org.testng.annotations.Test;
+
+public class Maven {
+@Test
+public void practice() {
+	System.out.println("apple");
+	System.out.println("Ball");
+}
+}
